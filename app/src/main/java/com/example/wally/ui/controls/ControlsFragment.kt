9a29bd95b72a6f07abdf65ee36c1595f8080f672
Tiger Hao <tiger.hao@uwaterloo.ca>
2022-01-31 19:25:29 -0500

    add initial task and control pages

--- app/src/main/java/com/example/wally/ui/controls/ControlsFragment.kt
+++ app/src/main/java/com/example/wally/ui/controls/ControlsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.wally.ui.dashboard
+package com.example.wally.ui.controls
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.wally.R
-import com.example.wally.databinding.FragmentDashboardBinding
+import com.example.wally.databinding.FragmentControlsBinding
 
-class DashboardFragment : Fragment() {
+class ControlsFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var controlsViewModel: ControlsViewModel
+    private var _binding: FragmentControlsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +25,14 @@ class DashboardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        controlsViewModel =
+                ViewModelProvider(this).get(ControlsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentControlsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textConstrolss
+        controlsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/wally/ui/controls/ControlsViewModel.kt
+++ app/src/main/java/com/example/wally/ui/controls/ControlsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.wally.ui.notifications
+package com.example.wally.ui.controls
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ControlsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is controls Fragment"
     }
     val text: LiveData<String> = _text
 }
