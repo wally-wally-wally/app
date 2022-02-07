fed0ea4471a912bd649d1bd0b5ff4484b79320e5
Andrea M <a62marti@uwaterloo.ca>
2022-02-06 23:47:46 -0500

    Add Bluetooth serial interface

--- app/src/main/java/bluetooth/request/IBluetoothRequest.kt
+++
@@ -1,5 +0,0 @@
-package bluetooth.request
-
-interface IBluetoothRequest {
-    fun cleanup()
-}
---
+++ app/src/main/java/com/example/wally/ui/bluetooth/BluetoothFragment.kt
@@ -0,0 +1,41 @@
+package com.example.wally.ui.bluetooth
+
+import android.bluetooth.BluetoothDevice
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.wally.databinding.FragmentBluetoothBinding
+
+
+class BluetoothFragment : Fragment() {
+    private lateinit var bluetoothViewModel: BluetoothViewModel
+    private var _binding: FragmentBluetoothBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        bluetoothViewModel =
+            ViewModelProvider(this).get(BluetoothViewModel::class.java)
+
+        _binding = FragmentBluetoothBinding.inflate(inflater, container, false)
+
+        bluetoothViewModel.setupBluetooth()
+        //bluetoothViewModel.refreshPairedDevices() //maybe? depends on how we're doing things in the app
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
