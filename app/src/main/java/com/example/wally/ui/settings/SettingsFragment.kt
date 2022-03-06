87f130e37268a09340f55735bdfdfd5a3dbbbd09
Tiger Hao <tiger.hao@uwaterloo.ca>
2022-03-06 03:33:49 -0500

    constrain controls layout

--- app/src/main/java/com/example/wally/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/wally/ui/settings/SettingsFragment.kt
@@ -1,13 +1,6 @@
 package com.example.wally.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.wally.R
 import androidx.preference.PreferenceFragmentCompat
 
@@ -15,36 +8,4 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
-//
-//    private lateinit var settingsViewModel: SettingsViewModel
-//    private var _binding: FragmentSettingsBinding? = null
-//
-//    // This property is only valid between onCreateView and
-//    // onDestroyView.
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        settingsViewModel =
-//            ViewModelProvider(this).get(SettingsViewModel::class.java)
-//
-//        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-//
-//        binding?.bluetoothButton?.setOnClickListener {
-//            connectToBluetooth()
-//        }
-//
-//        return binding.root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
-//
-//    private fun connectToBluetooth() {
-//    }
 }
